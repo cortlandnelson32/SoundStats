95c02f436d149e829cf32345a335dfc3a067b4a8
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-11-21 08:55:15 -0500

    init express and sequelize

---
+++ backend/routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const csrf = require('csurf');
+
+const csrfProtection = csrf({ cookie: true });
+
+router.use(csrfProtection);
+
+// //hello world test
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
+});
+
+module.exports = router;
