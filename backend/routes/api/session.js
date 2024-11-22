3719f8a23a6aab50f6bb366a20d5018100ebe077
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-11-22 12:53:11 -0500

    done validate login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,11 +4,24 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation')
+
+const validateLogin = [
+  check('credential')
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage('Email or username is required'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Password is required'),
+  handleValidationErrors
+];
 
 // Log in
 router.post(
   '/',
-  // validateLogin,
+  validateLogin,
   async (req, res, next) => {
     const { credential, password } = req.body;
 
