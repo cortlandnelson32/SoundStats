835acaf202015fc116b0e9f96c78d6521cbfa70c
Cortland Nelson <cortland.nelson2018@gmail.com>
2024-11-22 08:50:26 -0500

    done auth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,34 @@
 const router = require('express').Router();
+const { restoreUser, requireAuth, setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.use(restoreUser);
 
+//test setting the token cookie
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: 'DemoUser',
+    },
+  });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
 
-router.post('/test', function(req, res) {
+// restore user
+router.use(restoreUser);
+
+// test restored user
+router.get('/restore-user', (req, res) => {
+  return res.json(req.user);
+});
+
+// test req auth
+router.get('/require-auth', requireAuth, (req, res) => {
+  return res.json(req.user);
+});
+
+// Test POST route
+router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
